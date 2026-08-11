69baf363f9660cdcdf037fe58d1ef9e5c4811fda
Isfaqul <isfaq.xperia@gmail.com>
2026-08-11 13:03:33 +0530

    chore:add message to loading spinner

--- apps/user/src/components/LoadingSpinner.tsx
+++ apps/user/src/components/LoadingSpinner.tsx
@@ -3,7 +3,12 @@ import { AiOutlineLoading3Quarters } from "react-icons/ai";
 function LoadingSpinner() {
   return (
     <div className="h-full flex items-center justify-center">
-      <AiOutlineLoading3Quarters className="text-tGray-100 text-2xl animate-loading" />
+      <div className="flex flex-col items-center">
+        <p className="text-xs mb-2 text-white font-mono">
+          I hope this free tier wait time does not get me rejected :)
+        </p>
+        <AiOutlineLoading3Quarters className="text-tGray-100 text-2xl animate-loading" />
+      </div>
     </div>
   );
 }
